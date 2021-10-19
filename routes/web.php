50ba17b9fba721b51de0ba8ebea1bd3fc910797c
Azizsurya77 <azizsurya77@gmail.com>
2021-10-19 09:49:53 +0700

    Tugas 2: Base Admin Template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('admindashboard');
+});
+
+Route::get('/master', function () {
+    return view('adminapp.master');
+});
